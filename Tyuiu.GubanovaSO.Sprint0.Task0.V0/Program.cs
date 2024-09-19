1c0aba465d55d2b54395a67035be8670d03c2522
Софья <@NENEIE>
2024-09-19 19:08:05 +0500

    Add project files.

---
+++ Tyuiu.GubanovaSO.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GubanovaSO.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
