7e85edf5736bc2dcac6af23c2719895ec9a27e22
Tyuiu.GubanovaSO@outlook.com <neneie0701@gmail.com>
2024-09-19 19:40:17 +0500

    Task1

---
+++ Tyuiu.GubanovaSO.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.GubanovaSO.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Софья";
+            int age = 19;
+            Console.WriteLine("Здравствуйте, меня зовут "+name+ ", мне "+age+ " лет!");
+            Console.ReadKey();
+        }
+    }
+}
